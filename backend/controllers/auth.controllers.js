589d839f10273f9faebc26fef0cd90371300f3e0
CinouAchilles <yacinesa05@gmail.com>
2025-01-11 00:00:36 +0100

    Add logout funcionality

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -107,7 +107,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "logout hitted successfully",
-  });
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({message: "logged out successfully"});
+    } catch (error) {
+        console.error("Error in the Logout controller: "+ error.message);
+        res.status(500).json({error: "Internal Server Error"})
+    }
 };
